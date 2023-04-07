9ade1d5afe7c5c8597b7152a07eb988bb4d957bb
Manpreet Singh <manpreet123singh987@gmail.com>
2023-04-07 21:59:55 +0530

    modified data variable.py

--- Variables and Data types.py
+++ Variables and Data types.py
@@ -3,6 +3,6 @@ b=4
 a1="Manpreet"
 b1="Singh"
 
-print("the sum of a and b is=",a+b)
-print("the combination of a1 and b1 is=", a1+b1)
+print("the sum of a and b is=",a+b,type(a1))
+print("the combination of a1 and b1 is=", a1 + b1,type(b))
 
