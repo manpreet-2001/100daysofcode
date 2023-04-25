7a987ec02a8b7fea3bcb2a68fc3244b775d8bbe1
Manpreet Singh <manpreet123singh987@gmail.com>
2023-04-25 20:17:16 +0530

    modified

--- Variables and Data types.py
+++ Variables and Data types.py
@@ -1,8 +1,10 @@
 a= 6
 b=4
+print("the sum of a and b is=",a+b,type(a))
+
 a1="Manpreet"
 b1="Singh"
+print("the combination of a1 and b1 is=", a1 + b1,type(b1))
 
-print("the sum of a and b is=",a+b,type(a1))
-print("the combination of a1 and b1 is=", a1 + b1,type(b))
-
+c=complex(5,3)
+print (c)
