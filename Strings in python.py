8416b475724ca86cad08397112be7281560a6bf1
Manpreet Singh <manpreet123singh987@gmail.com>
2023-06-06 23:47:38 +0530

    add string on 06/06/23

---
+++ Strings in python.py
@@ -0,0 +1,15 @@
+#String : Something enclose in single or double qoutation that is string
+
+name= "Manpreet"
+print("hey",name)
+
+#suppose we write something double qoute in string
+con='today weather is "great"'
+print(con)
+
+#if we want to display add of multiple line
+data='''today's weather is "great"
+Tell me something about yourself'''
+print(data)
+
+print(con[1])
